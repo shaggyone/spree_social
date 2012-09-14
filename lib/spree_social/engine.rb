141d8f38ce814cac1c6d0c644b490d04c4b10d1d
Victor Zagorski <victor@zagorski.ru>
2012-09-14 10:12:50 +0800

    Added vkontakte oauth provider
    Conflicts:
    
    	lib/spree_social/engine.rb

--- lib/spree_social/engine.rb
+++ lib/spree_social/engine.rb
@@ -3,7 +3,8 @@ module SpreeSocial
     ["Facebook", "facebook"],
     ["Twitter", "twitter"],
     ["Github", "github"],
-    ["Google", "google_oauth2"]
+    ["Google", "google_oauth2"],
+    ["Vkontakte", "vkontakte"]
   ]
 
   class Engine < Rails::Engine
